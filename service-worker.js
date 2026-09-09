ae4f0001b5a364f08afa20063d00fdca9da160a5
Edisonhuang22 <484784621@qq.com>
2026-09-09 16:40:21 +0800

    Deploy finance dashboard and offline shell

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE = 'yibi-static-v1';
+const CORE = ['./', './manifest.webmanifest', './favicon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => {
+    const update = fetch(event.request).then(response => {
+      if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+      return response;
+    }).catch(() => cached);
+    return cached || update;
+  }));
+});
